34999ba42f2d337ff13654e2dca84186ae347322
unknown <rmmccar92@gmail.com>
2023-06-02 17:36:20 -0400

    fixed studnet name bug

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -1,18 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import data from "../../../data/people.json";
 
 interface PageProps {
   params: {
-    id: number;
+    id: string;
   };
   searchParams: URLSearchParams;
 }
 
 export default function Page({ params, searchParams }: PageProps) {
-  const student = data.find((person) => params.id === params.id);
-  if (!student) {
-    return <div>Student not found</div>;
-  }
-  const nameArr = student.name.split(" ");
+  const parsedID = parseInt(params.id);
+  const student: String = data.find(({ id }) => id === parsedID);
+  const nameArr = student?.name.split(" ");
   const firstName = nameArr[1];
   const lastName = nameArr[0].slice(0, -1);
   const fullName = firstName + " " + lastName;

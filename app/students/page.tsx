4e8e7bea6ba70a62b88831413e08bb1ac8e77116
unknown <rmmccar92@gmail.com>
2023-06-30 12:54:52 -0400

    started working with use-gesture and react-spring

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import data from "../../data/people.json";
 import styles from "../styles/studentlist.module.css";
 import Image from "next/image";
@@ -7,18 +7,54 @@ import GroupsComponent from "../components/Groups";
 import StudentModal from "../components/StudentModal";
 import GroupsPanel from "../components/GroupsPanel.tsx";
 import type { Student, Group } from "../../types.ts";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useSprings, animated } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
+
+const animationFn =
+  (
+    order: number[],
+    active: boolean = false,
+    originalIndex: number = 0,
+    curIndex: number = 0,
+    y: number = 0,
+    x: number = 0
+  ) =>
+  // DOUBLE ARROW FUNCTION :O
+  (index: number) =>
+    active && index === originalIndex
+      ? {
+          y: curIndex * 100 + y,
+          x: curIndex * 100 + x,
+          scale: 1.1,
+          zIndex: "1",
+          shadow: 15,
+          // determines which properties are animated immediately
+          immediate: (key: string) =>
+            key === "y" || key === "zIndex" || key === "x",
+        }
+      : {
+          y: order.indexOf(index) * 50,
+          x: order.indexOf(index) * 50,
+          scale: 1,
+          zIndex: 0,
+          shadow: 1,
+          immediate: false,
+        };
 
 const StudentsPage = () => {
   let value: any = [];
-  // Get the value from local storage if it exists
 
+  // State variables
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
   const [toggleGroupPanel, setToggleGroupPanel] = useState<boolean>(false);
-
   const [groups, setGroups] = useState<Group[] | []>([]);
+  // animation coordinates
+
+  // Get values from local storage if they exist
   useEffect(() => {
+    console.log("Springs", springs);
+
     if ("groups" in localStorage) {
       value = JSON.parse(localStorage.getItem("groups") as string);
       // console.log(value);
@@ -27,28 +63,38 @@ const StudentsPage = () => {
     }
     setGroups(value);
   }, []);
-
+  // Remove instructors from the list
   const students = data.filter(
     (person) => person.enrollments[0].type === "StudentEnrollment"
   );
 
-  const dragEndHandler = (result: any) => {
-    console.log("RESULT", result);
-    const { destination, source, draggableId } = result;
-    if (!destination) {
-      return;
-    }
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    } else {
-      console.log("DESTINATION", destination);
-      addGroupMember(destination.droppableId, draggableId);
-    }
+  const order = useRef(students.map((_, index) => index));
+  const [springs, api] = useSprings(
+    students.length,
+    animationFn(order.current)
+  );
+
+  const swap = (arr: number[], index1: number, index2: number): number[] => {
+    return ([arr[index1], arr[index2]] = [arr[index2], arr[index1]]);
   };
 
+  const clamp = (number: number, min: number, max: number) =>
+    Math.max(min, Math.min(number, max));
+
+  const bind = useDrag(
+    ({ args: [originalIndex], active, movement: [x, y] }) => {
+      const curIndex = order.current.indexOf(originalIndex);
+      const curRow = clamp(
+        Math.round((curIndex * 100 + y) / 100),
+        0,
+        students.length - 1
+      );
+      const newOrder = swap(order.current, curIndex, curRow);
+      api.start(animationFn(newOrder, active, originalIndex, curIndex, y, x));
+      // Feed springs new style data, they'll animate the view without causing a single render
+      if (!active) order.current = newOrder;
+    }
+  );
   const addGroupMember = (groupId: string, studentId: string) => {
     groups.forEach((group) => {
       const realGroupId = groupId.split("-")[0];
@@ -130,75 +176,64 @@ const StudentsPage = () => {
         </>
       )}
       <h1 className={styles.title}>Students</h1>
-      {/* <DragDropContext onDragEnd={dragEndHandler}> */}
       <div className={styles.studentPage}>
-        {/* <Droppable droppableId={"student-list"}>
-            {(provided, snapShot) => ( */}
         <ul
-          // {...provided.droppableProps}
-          // ref={provided.innerRef}
-          role="listitem"
           className={toggleGroupPanel ? styles.panelOpen : styles.studentList}
         >
-          {/* {provided.placeholder} */}
-          {students?.map(
-            (student, i) => (
-              // <Draggable
-              //   draggableId={student.id.toString()}
-              //   key={student.id}
-              //   index={i}
-              // >
-              // {(provided, snapShot) => (
-              <li
-                // {...provided.draggableProps}
-                // {...provided.dragHandleProps}
-                key={i}
-                className={styles.listItem}
-                // ref={provided.innerRef}
-                // @ts-ignore
-                // isDragging={snapShot.isDragging}
-                onClick={() => openModal(student.id)}
-              >
-                <div className={styles.buttonInfo}>
-                  {student.avatar_url ? (
-                    <Image
-                      className={styles.avatar}
-                      src={student.avatar_url}
-                      alt="Student Avatar"
-                      width={40}
-                      height={40}
-                      // priority
-                    />
-                  ) : (
-                    <Image
-                      className={styles.avatar}
-                      src="/vercel.svg"
-                      alt="Student Avatar"
-                      width={50}
-                      height={50}
-                      // priority
-                    />
-                  )}
-                  <div className={styles.buttonContent}>{student.name}</div>
-                  <div className={styles.buttonLogo}>
-                    <Image
-                      src="/unc.png"
-                      alt="UNC Logo"
-                      width={75}
-                      height={75}
-                      // priority
-                    />
-                  </div>
+          {/* {students?.map((student, i) => (
+            <div key={i}> */}
+          {springs.map(({ zIndex, shadow, y, x, scale }, i) => (
+            <animated.li
+              key={i}
+              {...bind(i)}
+              style={{
+                zIndex,
+                boxShadow: shadow.to(
+                  (s) => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
+                ),
+                y,
+                x,
+                scale,
+              }}
+              className={styles.listItem}
+              // onClick={() => openModal(student.id)}
+            >
+              <div className={styles.buttonInfo}>
+                {/* {student.avatar_url ? ( */}
+                {/* <Image
+                        className={styles.avatar}
+                        src={student.avatar_url}
+                        alt="Student Avatar"
+                        width={40}
+                        height={40}
+                        // priority
+                      />
+                    ) : (
+                      <Image
+                        className={styles.avatar}
+                        src="/vercel.svg"
+                        alt="Student Avatar"
+                        width={50}
+                        height={50}
+                        // priority
+                      />
+                    )} */}
+                {/* <div className={styles.buttonContent}>{student.name}</div> */}
+                <div className={styles.buttonLogo}>
+                  <Image
+                    src="/unc.png"
+                    alt="UNC Logo"
+                    width={75}
+                    height={75}
+                    // priority
+                  />
                 </div>
-              </li>
-            )
-            // }
-            // </Draggable>
-            // )
-          )}
+              </div>
+            </animated.li>
+          ))}
+          {/* </div> */}
+          {/* ))} */}
         </ul>
-        {/* )} */}
-        {/* </Droppable> */}
       </div>
       <div className={styles.groupButton}>
         <GroupsComponent
@@ -215,7 +250,6 @@ const StudentsPage = () => {
           // dragEndHandler={dragEndHandler}
         />
       )}
-      {/* </DragDropContext> */}
     </>
   );
 };

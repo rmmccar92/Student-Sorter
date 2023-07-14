c4736520c3ebf469d62f947d14acd7d60d957afc
unknown <rmmccar92@gmail.com>
2023-07-14 14:52:02 -0400

    deploy?

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -1,21 +1,21 @@
 // "use client";
 
-// import { useEffect, useState } from "react";
-import data from "../../../data/people.json";
-import { Student } from "../../../types.ts";
-interface PageProps {
-  params: {
-    id: string;
-  };
-  searchParams: URLSearchParams;
-}
+// // import { useEffect, useState } from "react";
+// import data from "../../../data/people.json";
+// import { Student } from "../../../types.ts";
+// interface PageProps {
+//   params: {
+//     id: string;
+//   };
+//   searchParams: URLSearchParams;
+// }
 
-export default function Page({ params, searchParams }: PageProps) {
-  const parsedID = parseInt(params.id);
-  const student: any = data.find(({ id }) => id === parsedID);
-  const nameArr = student?.name.split(" ");
-  const firstName = nameArr[1];
-  const lastName = nameArr[0].slice(0, -1);
-  const fullName = firstName + " " + lastName;
-  return <div>Hi! {fullName}</div>;
-}
+// export default function Page({ params, searchParams }: PageProps) {
+//   const parsedID = parseInt(params.id);
+//   const student: any = data.find(({ id }) => id === parsedID);
+//   const nameArr = student?.name.split(" ");
+//   const firstName = nameArr[1];
+//   const lastName = nameArr[0].slice(0, -1);
+//   const fullName = firstName + " " + lastName;
+//   return <div>Hi! {fullName}</div>;
+// }
